062f67ffc4e92a937ce1a94b8e13559e5f47ac64
JeffChau0503 <35581563+JeffChau0503@users.noreply.github.com>
2018-02-01 20:05:44 +0100

    修改了2to3 的语法问题

--- Perceptron.py
+++ Perceptron.py
@@ -1,36 +1,38 @@
-class Perceptron(object):            #感知器
-    def __init__(self,input_num,activator): #输入，激活函数
-        '''
+class Perceptron(object):  # 感知器
+    def __init__(self, input_num, activator):  # 输入个数，激活函数
+        """
         初始化感知器
         :param input_num:输入参数的个数
         :param activator:激活函数的类型 double -> double
-        '''
+        """
         self.activator = activator
-        #权重向量初始化为0
-        self.weights = [0.0 for _ in range(input_num)] #该数组被 0.0 填充
-        #偏置项初始化为0
+        # 权重向量初始化为0
+        self.weights = [0.0 for _ in range(input_num)]  # 该数组被 0.0 填充
+        # 偏置项初始化为0
         self.bias = 0.0
+
     def __str__(self):
         """
         打印学习到的权重，偏置项。
         """
-        return 'weight\t:%s\nbias\t:%f\n' %(self.weights,self.bias)
+        return 'weight\t:%s\nbias\t:%f\n' % (self.weights, self.bias)
+
     def predict(self, input_vec):
         """
         :param input_vec:输入向量
         :return:输出感知器的计算结果
         """
-        #把input_vec[x1,x2,x3...]和weight[w1,w2,w3...]打包一起 zip()
-        #变成[(x1,w1),(x2,w2),(x3,w3),...]
-        #然后利用map函数计算[x1*w1,x2*w2,x3*w3]
-        #最后利用reduce求和
-        from functools import reduce #从fucntools模块中引用
+        #  把input_vec[x1,x2,x3...]和weight[w1,w2,w3...]打包一起 zip()
+        #  变成[(x1,w1),(x2,w2),(x3,w3),...]
+        #  然后利用map函数计算[x1*w1,x2*w2,x3*w3]
+        #  最后利用reduce求和
+        from functools import reduce  # 从fucntools模块中引用
         return self.activator(
-            reduce(lambda a,b: a+b,
-                    map(lambda x,w: x * w, zip(input_vec, self.weights))
-             , 0.0)
-                   + self.bias)
-    def  train(self,input_vecs,labels,iteration,rate):
+            reduce(lambda a, b: a+b,
+                   [x_w[0] * x_w[1] for x_w in zip(input_vec, self.weights)], 0.0) + self.bias)
+        # 0.0这里作为reduce的初始值
+
+    def train(self, input_vecs, labels, iteration, rate):
         """
         输入训练数据：
         :param input_vecs:一组向量
@@ -40,33 +42,32 @@ class Perceptron(object):            #感知器
         :return:
         """
         for i in range(iteration):
-            self._one_iteration(input_vecs,labels,rate)
-    def _one_iteration(self,input_vecs,labels,rate):
+            self._one_iteration(input_vecs, labels, rate)
+
+    def _one_iteration(self, input_vecs, labels, rate):
             """
             一次迭代，把所有数据过一遍
             """
-            #把输入和输出打包在一起，成为样本的列表[(input_vec,label), ... ]
-            #而每个训练的样本是(input_vec,label)
+            #  把输入和输出打包在一起，成为样本的列表[(input_vec,label), ... ]
+            #  而每个训练的样本是(input_vec,label)
 
-            samples = zip(input_vecs,labels)
+            samples = list(zip(input_vecs, labels))
 
-            #对每个样本，按照感知器规则更新权重
-            for (input_vec,label) in samples:
-                #计算感知器在当前权重下的输出
+            #  对每个样本，按照感知器规则更新权重
+            for (input_vec, label) in samples:
+                #  计算感知器在当前权重下的输出
                 output = self.predict(input_vec)
-                #更新权重
-                self._update_weights(input_vec,output,label,rate)
+                #  更新权重
+                self._update_weights(input_vec, output, label, rate)
 
-    def _update_weights(self,input_vec, output, label, rate):
+    def _update_weights(self, input_vec, output, label, rate):
         """
         按照感知器的规则更新权重
         """
-        #把input_vec[x1,x2,x3,...]和weights[w1,w2,w3,...]打包在一起
-        #变成[(x1,w1),(x2,w2),(x3,w3),...]
-        #然后利用感知器规则更新权重
+        #  把input_vec[x1,x2,x3,...]和weights[w1,w2,w3,...]打包在一起
+        #  变成[(x1,w1),(x2,w2),(x3,w3),...]
+        #  然后利用感知器规则更新权重
         delta = label - output
-        self.weights = map(
-            lambda x,w: w + rate * delta * x,
-            zip(input_vec, self.weights))
-        # 更新bias
+        self.weights = [x_w1[1] + rate * delta * x_w1[0] for x_w1 in zip(input_vec, self.weights)]
+        #  更新bias
         self.bias += rate * delta
--- and_fcn_DL.py
+++ and_fcn_DL.py
@@ -1,39 +1,43 @@
+from Perceptron import Perceptron  # 调用Perceptron文件
+
+
 def f(x):
-    '''
+    """
     定义激活函数f(x)
-    '''
-    return 1 if x>0 else 0 #注意return后面表达式写法
+    """
+    return 1 if x > 0 else 0  # 注意return后面表达式写法
+
 
 def get_training_detaset():
-    '''
+    """
     基于真值表构建训练数据
-    '''
-    input_vecs = [[1,1],[0,0],[1,0],[0,1]]
-    labels = [1,0,0,0]
-    return input_vecs,labels
+    """
+    input_vecs = [[1, 1], [0, 0], [1, 0], [0, 1]]
+    labels = [1, 0, 0, 0]
+    return input_vecs, labels
+
 
 def train_and_perceptron():
-    '''
+    """
     使用and真值表训练感知器
-    '''
-    #调用Perceptron文件
-    import Perceptron
-    Perceptron.Perceptron()
-    #创建感知器，输入参数个数为2（因为and是一个二元函数），激活函数为f
-    p = Perceptron(2,f)
-    #训练，迭代10次，学习速率为0.1
-    input_vecs,labels = get_training_detaset()
-    p.train(input_vecs,labels,10,0.1)
-    #返回训练好的感知器
+    """
+    #  创建感知器，输入参数个数为2（因为and是一个二元函数），激活函数为f
+    p = Perceptron(2, f)
+    #  训练，迭代10次，学习速率为0.1
+    input_vecs, labels = get_training_detaset()
+    p.train(input_vecs, labels, 10, 0.1)
+    #  返回训练好的感知器
     return p
 
-if __name__=='__main__':
-    #训练and感知器
+
+if __name__ == '__main__':
+    # 训练and感知器
     and_perception = train_and_perceptron()
     # 打印训练获得的权重
     print(and_perception)
-    #测试
-    print('1 and 1 = %d' % and_perception.predict([1, 1]))
-    print('0 and 0 = %d' % and_perception.predict([0, 0]))
-    print('1 and 0 = %d' % and_perception.predict([1, 0]))
-    print('0 and 1 = %d' % and_perception.predict([0, 1]))
+
+    # 测试
+    print(('1 and 1 = %d' % and_perception.predict([1, 1])))
+    print(('0 and 0 = %d' % and_perception.predict([0, 0])))
+    print(('1 and 0 = %d' % and_perception.predict([1, 0])))
+    print(('0 and 1 = %d' % and_perception.predict([0, 1])))
