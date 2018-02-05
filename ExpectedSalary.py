7530b58c680c53e66da9525425dabbd90df072c2
JeffChau0503 <35581563+JeffChau0503@users.noreply.github.com>
2018-02-05 16:15:30 +0100

    修改了显示错误

--- ExpectedSalary.py
+++ ExpectedSalary.py
@@ -18,7 +18,7 @@ def train_linear_unit():
     使用数据训练线性单元
     """
     #  创建感知器，输入参数的特征数为1(工作年限)
-    lu = Linearunit(1)
+    lu = Linearunit(1)  # lu是LinearUnit的缩写
     #  训练，迭代10轮，学习速率0.01
     input_vecs, labels = get_training_detaset()
     lu.train(input_vecs, labels, 10, 0.01)
@@ -32,6 +32,6 @@ if __name__ == '__main__':
     print(linear_unit)
     #  测试
     print('work 3.4 years, monthly salary = %.2f' % linear_unit.predict([3.4]))
-    print('work 3.4 years, monthly salary = %.2f' % linear_unit.predict([15]))
-    print('work 3.4 years, monthly salary = %.2f' % linear_unit.predict([1.5]))
-    print('work 3.4 years, monthly salary = %.2f' % linear_unit.predict([6.3]))
+    print('work 15  years, monthly salary = %.2f' % linear_unit.predict([15]))
+    print('work 1.5 years, monthly salary = %.2f' % linear_unit.predict([1.5]))
+    print('work 6.3 years, monthly salary = %.2f' % linear_unit.predict([6.3]))
